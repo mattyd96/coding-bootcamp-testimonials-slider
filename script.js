0674a561bb86879c8dd94462a91ae5ed7c7957c0
Matthew <furikuribeebop@gmail.com>
2021-06-27 17:47:30 +0930

    mobile and desktop layout finished

--- script.js
+++ script.js
@@ -1,48 +1,42 @@
+//psuedo database
+var slides = [
+    {
+        image: "images/image-tanya.jpg",
+        text: "\“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. \”",
+        name: "Tanya Sinclair",
+        title: "UX Engineer"
+    },
+    {
+        image:"images/image-john.jpg",
+        text: "\“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. \”",
+        name: "John Tarkpor",
+        title: "Junior Front-end Developer"
+    }  
+]
+
 var slideIndex = 0;
 
+//go to next slide
 function nextSlide () {
     slideIndex += 1;
     console.log(slideIndex);
     showSlide();
 }
 
+//go to previous slide
 function prevSlide () {
     slideIndex -= 1;
     showSlide();
 }
 
-function showSlide () {
-    //get slides
-    var slides = document.getElementsByClassName('testimony');
-
-    //make sure slide index is withing range (loop around)
+function showSlide() {
+    //make sure slideIndex stays in bounds and loops around
     if(slideIndex < 0) {slideIndex = slides.length - 1}
     else if(slideIndex >= slides.length) {slideIndex = 0}
 
-    //set all slides to opaque
-    Array.from(slides).forEach(element => {
-        element.style.opacity = "0";
-    });
-
-    //show current slide
-    slides[slideIndex].style.opacity = "1";
+    //replace all necessary elements with the next slides information
+    document.getElementById("pic-replace").src = slides[slideIndex].image;
+    document.getElementById("text-replace").innerText = slides[slideIndex].text;
+    document.getElementById("name-replace").innerText = slides[slideIndex].name;
+    document.getElementById("title-replace").innerText = slides[slideIndex].title;
 }
-
-// Add event listener on keydown for slide changes
-document.addEventListener('keydown', (event) => {
-    var name = event.key;
-
-    if(name == "ArrowRight") {
-        nextSlide();
-    }
-    else if(name == "ArrowLeft") {
-        prevSlide();
-    }
-
-}, false);
-
-window.onload = (event) => {
-    console.log('The page has fully loaded');
-    var slides = document.getElementsByClassName('testimony');
-    slides[0].style.opacity = "1";
-};
